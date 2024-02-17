75aa2e82eef5e8f924c92fbd857b570f27ad1f4a
shivam balayan <shivambalayan@shivams-MacBook-Pro.local>
2024-02-18 00:30:36 +0530

    added navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import NavbarComp from "./Navbar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <NavbarComp />
+    </div>
+  );
 };
 
 export default Home;
