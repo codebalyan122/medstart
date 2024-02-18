149e252a399ee303e884a3ccdf4ab688b6cbf496
shivam balayan <shivambalayan@shivams-MacBook-Pro.local>
2024-02-18 22:46:20 +0530

    addded all medicines

--- src/components/AllMedicines.jsx
+++ src/components/AllMedicines.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useQuery } from "@tanstack/react-query";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -12,15 +13,18 @@ function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 export default function AllMedicines() {
+  const { isFetching, error, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () =>
+      fetch("https://med-lyhk.onrender.com/medicine").then((res) => res.json()),
+  });
+
+  if (isFetching) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+  console.log(data);
+  // If data is available, render the table
   return (
     <>
       <NavbarComp />
@@ -28,26 +32,26 @@ export default function AllMedicines() {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>MEDICINE NAME</TableCell>
+              <TableCell align="right">COMPOSITION</TableCell>
+              <TableCell align="right">MRP&nbsp;(g)</TableCell>
+              <TableCell align="right">SELLING PRICE&nbsp;(g)</TableCell>
+              <TableCell align="right">UNIT&nbsp;(g)</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {data.map((row, index) => (
               <TableRow
-                key={row.name}
+                key={index}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {row.medicine.medicine_name}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.medicine.composition}</TableCell>
+                <TableCell align="right">{row.mrp}</TableCell>
+                <TableCell align="right">{row.selling_price}</TableCell>
+                <TableCell align="right">{row.unit}</TableCell>
               </TableRow>
             ))}
           </TableBody>
